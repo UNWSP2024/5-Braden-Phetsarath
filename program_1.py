e30f0b207c2723b628cc408211f9c227e4051f79
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-09-30 16:27:12 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,19 +1,24 @@
 # Program #1: Kilometer Converter
-# Write a program that asks the user to enter a distance in kilometers, 
-# then converts that distance to miles.  The conversion formula is as follows:  
-# Miles = kilometers x 0.6214.   
+# Write a program that asks the user to enter a distance in kilometers,
+# then converts that distance to miles.  The conversion formula is as follows:
+# Miles = kilometers x 0.6214.
 # The conversion must be done as a function with input and output.
 
+#Braden Phetsarath
+#9/30
+#Kilometer to mile conversion
 
-def kilometer_conversion(kilometers):    
+def kilometer_conversion(kilometers):
     miles = 0.0
     ######################
     # WRITE YOUR CODE HERE
-    ######################    
-
+    miles = kilometers * 0.621371
+    print ("this is how mar away it is in miles", miles)
+    ######################
 
     # Return the variable to the calling function
     return miles
+
 
 #### This piece of the code has been done for you,
 #### you only need to worry about the actual kilometer
@@ -21,6 +26,7 @@ def kilometer_conversion(kilometers):
 if __name__ == '__main__':
     # Get User Input
     print('in main')
+    kilometers = int(input("How far away is it in Kilometers? : "))
+    kilometer_conversion(kilometers)
     # Call kilometer_conversion, don't forget to pass in the kilometer parameter!
-    
     # Display the miles
